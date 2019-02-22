796fd5784410863ac28285a6931b60b2d9633236
dongyangyang1 <dyydeyou@126.com>
2019-02-23 00:20:01 +0800

    提交bendi

---
+++ src/com/itheima/Test.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(8888);
+        System.out.println(8888);
+        System.out.println(8888);
+        System.out.println(8888);
+        System.out.println(8888);
+    }
+}
